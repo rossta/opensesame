a374fcccd76dd5b31bbe366a7d21b2e2336476c9
Ross Kaffenberger <rosskaff@gmail.com>
2012-08-14 22:33:39 -0400

    dummy strings for dummy app client configuration

--- spec/dummy/config/initializers/opensesame.rb
+++ spec/dummy/config/initializers/opensesame.rb
@@ -1,7 +1,20 @@
 require "opensesame"
 
+app_id = ENV['SESAMESTREET_APP_ID']
+secret = ENV['SESAMESTREET_SECRET']
+
+if app_id.nil?
+  puts "Setting app_id to dummy string"
+  app_id = 'dummy_app_id'
+end
+
+if secret.nil?
+  puts "Setting secret to dummy string"
+  secret = 'dummy_secret'
+end
+
 OpenSesame.configure do |config|
   config.organization 'challengepost'
   config.mounted_at   '/opensesame'
-  config.client       ENV['SESAMESTREET_APP_ID'], ENV['SESAMESTREET_SECRET']
+  config.client       app_id, secret
 end
