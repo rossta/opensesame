c40a9684b0431fe519234cbbe3979a8d68f37be7
Ross Kaffenberger <rosskaff@gmail.com>
2012-08-21 11:32:25 -0400

    try env vars for spec dummy initializer

--- spec/dummy/config/initializers/opensesame.rb
+++ spec/dummy/config/initializers/opensesame.rb
@@ -1,8 +1,21 @@
 require "opensesame"
 
+app_id = ENV['GITHUB_APP_ID']
+secret = ENV['GITHUB_SECRET']
+
+if app_id.nil?
+  puts "Setting app_id to dummy string"
+  app_id = 'dummy_app_id'
+end
+
+if secret.nil?
+  puts "Setting secret to dummy string"
+  secret = 'dummy_secret'
+end
+
 OpenSesame.configure do |config|
   config.enable       true
   config.organization 'challengepost'
   config.mounted_at   '/opensesame'
-  config.github       'github_client_id', 'github_client_secret'
+  config.github       app_id, secret
 end
