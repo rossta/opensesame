627ac62ca704bd43a73bbcf6507341fbc6c95312
Ross Kaffenberger <rosskaff@gmail.com>
2012-08-28 16:20:28 -0400

    override failure app

--- lib/open_sesame/github_auth.rb
+++ lib/open_sesame/github_auth.rb
@@ -6,5 +6,20 @@ module OpenSesame
     option :name, 'github'
     option :path_prefix, OpenSesame.mount_prefix
     option :on_failure, OpenSesame::Failure::App.new
+
+    # overrides OmniAuth::Strategy#fail!
+    def fail!(message_key, exception = nil)
+      self.env['omniauth.error'] = exception
+      self.env['omniauth.error.type'] = message_key.to_sym
+      self.env['omniauth.error.strategy'] = self
+
+      if exception
+        log :error, "Authentication failure! #{message_key}: #{exception.class.to_s}, #{exception.message}"
+      else
+        log :error, "Authentication failure! #{message_key} encountered."
+      end
+
+      options.on_failure.call(self.env)
+    end
   end
 end
